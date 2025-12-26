4804ba16577276906f1fa2bcc8fd1b9bca912398
Jeff Weeks <jweeks@neuraldk.org>
2025-12-25 22:46:18 -0500

    Isolate graphics code to separate file
    This code shouldn't need to know how the
    screen/window/whatever was created, it just knows
    how to draw into the provided buffer.

---
+++ src/gfx.rs
@@ -0,0 +1,86 @@
+pub struct Screen<'a> {
+    pub buffer: &'a mut [u8],
+    pub width: usize,
+    pub height: usize,
+    pub bytes_per_pixel: usize,
+    pub bytes_per_line: usize,
+}
+
+impl<'a> Screen<'a> {
+    // TODO: these functions need to accept a colour
+    pub fn putpixel(&mut self, x: usize, y: usize) {
+        let offset = y * self.bytes_per_line + (x*self.bytes_per_pixel);
+        if x < self.width && y < self.height {
+            self.buffer[offset] = 0xff;
+            self.buffer[offset+1] = 0xff;
+            self.buffer[offset+2] = 0xff;
+        }
+    }
+
+    // Rust translation of the Bresenham line agorithm
+    // http://neuraldk.org/document.php?djgppGraphics
+    pub fn line(&mut self, x1: usize, y1: usize, x2: usize, y2: usize) {
+        // locally convert to signed (and mutable) so that we can add the (also signed) direction to them
+        let mut x1: i32 = x1 as i32;
+        let mut y1: i32 = y1 as i32;
+        let mut x2: i32 = x2 as i32;
+        let mut y2: i32 = y2 as i32;
+
+        if(y1 > y2) {
+            y1 ^= y2; // swap y1 and y2
+            y2 ^= y1;
+            y1 ^= y2;
+            x1 ^= x2; // swap x1 and x2
+            x2 ^= x1;
+            x1 ^= x2;
+        }
+        let mut delta_x = x2 - x1;  // will determine L->R or R->L
+        let mut delta_y = y2 - y1;  // has to be positive because line goes T->B
+        let direction = match (delta_x > 0) { 
+            true => 1i32,           // delta_x is positive: we're going left to right
+            false => {              // delta_x is negative: we're going from right to left
+                delta_x = -delta_x; // we need the absolute length of this axis later on
+                -1i32
+            }
+        };
+    
+        match delta_x > delta_y { // what is our main axis
+            true => { // major axis is the x
+                let double_delta_y = delta_y + delta_y;
+                let diff_double_deltas = double_delta_y - (delta_x + delta_x);
+                let mut error = double_delta_y - delta_x;
+                self.putpixel(x1 as usize, y1 as usize); // plot our first pixel
+                //while(delta_x -= 1) { // loop for the length of the major axis
+                while delta_x > 0 {
+                    if(error >= 0) { // if the error is greater than or equal to zero:
+                    y1 += 1; // increase the minor axis (y)
+                    error += diff_double_deltas;
+                    } else {
+                        error += double_delta_y;
+                    }
+                    x1 += direction; // increase the major axis to next pixel
+                    self.putpixel(x1 as usize, y1 as usize); // plot our pixel
+                    delta_x -= 1;
+                }
+            }
+            false => { // major axis is the y
+                let double_delta_x = delta_x + delta_x;
+                let diff_double_deltas = double_delta_x - (delta_y + delta_y);
+                let mut error = double_delta_x - delta_y;
+                self.putpixel( x1 as usize, y1 as usize); // plot our first pixel
+                //while(delta_y -= 1) { // loop for the length of the major axis
+                while delta_y > 0 {
+                    if(error >= 0) { // if the error is greater than or equal to zero:
+                    x1 += direction; // increase the minor axis (x)
+                    error += diff_double_deltas;
+                    } else  {
+                        error += double_delta_x;
+                    }
+                    y1 += 1; // increase major axis to next pixel
+                    self.putpixel(x1 as usize, y1 as usize); // plot our pixel
+                    delta_y -= 1;
+                }
+            }
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,13 @@
 extern crate sdl2;
 extern crate trigr;
 
+mod gfx;
 mod simple_object;
 
-use trigr::SineCosineTable;
+use gfx::Screen;
 use simple_object::SimpleObject;
+use trigr::SineCosineTable;
+
 use std::ops;
 use std::fmt;
 
@@ -17,14 +20,6 @@ use std::time::SystemTime;
 static WIN_WIDTH: usize = 800;
 static WIN_HEIGHT: usize = 600;
 static FOV: usize = 60;
-
-struct Screen<'a> {
-    buffer: &'a mut [u8],
-    width: usize,
-    height: usize,
-    bytes_per_pixel: usize,
-    bytes_per_line: usize,
-}
 
 #[derive(Debug, Copy, Clone)]
 struct Point {
@@ -163,83 +158,6 @@ impl ops::Mul<Matrix> for Matrix {
     }
 }
 
-// TODO: these functions need to accept a colour
-pub fn putpixel(screen: &mut Screen, x: usize, y: usize) {
-    let offset = y * screen.bytes_per_line + (x*screen.bytes_per_pixel);
-    if x < screen.width && y < screen.height {
-        screen.buffer[offset] = 0xff;
-        screen.buffer[offset+1] = 0xff;
-        screen.buffer[offset+2] = 0xff;
-    }
-}
-
-// Rust translation of the Bresenham line agorithm
-// http://neuraldk.org/document.php?djgppGraphics
-pub fn line(screen: &mut Screen, x1: usize, y1: usize, x2: usize, y2: usize) {
-    // locally convert to signed (and mutable) so that we can add the (also signed) direction to them
-    let mut x1: i32 = x1 as i32;
-    let mut y1: i32 = y1 as i32;
-    let mut x2: i32 = x2 as i32;
-    let mut y2: i32 = y2 as i32;
-
-    if(y1 > y2) {
-        y1 ^= y2; // swap y1 and y2
-        y2 ^= y1;
-        y1 ^= y2;
-        x1 ^= x2; // swap x1 and x2
-        x2 ^= x1;
-        x1 ^= x2;
-    }
-    let mut delta_x = x2 - x1;  // will determine L->R or R->L
-    let mut delta_y = y2 - y1;  // has to be positive because line goes T->B
-    let direction = match (delta_x > 0) { 
-        true => 1i32,           // delta_x is positive: we're going left to right
-        false => {              // delta_x is negative: we're going from right to left
-            delta_x = -delta_x; // we need the absolute length of this axis later on
-            -1i32
-        }
-    };
- 
-    match delta_x > delta_y { // what is our main axis
-        true => { // major axis is the x
-            let double_delta_y = delta_y + delta_y;
-            let diff_double_deltas = double_delta_y - (delta_x + delta_x);
-            let mut error = double_delta_y - delta_x;
-            putpixel(screen, x1 as usize, y1 as usize); // plot our first pixel
-            //while(delta_x -= 1) { // loop for the length of the major axis
-            while delta_x > 0 {
-                if(error >= 0) { // if the error is greater than or equal to zero:
-                y1 += 1; // increase the minor axis (y)
-                error += diff_double_deltas;
-                } else {
-                    error += double_delta_y;
-                }
-                x1 += direction; // increase the major axis to next pixel
-                putpixel(screen, x1 as usize, y1 as usize); // plot our pixel
-                delta_x -= 1;
-            }
-        }
-        false => { // major axis is the y
-            let double_delta_x = delta_x + delta_x;
-            let diff_double_deltas = double_delta_x - (delta_y + delta_y);
-            let mut error = double_delta_x - delta_y;
-            putpixel(screen, x1 as usize, y1 as usize); // plot our first pixel
-            //while(delta_y -= 1) { // loop for the length of the major axis
-            while delta_y > 0 {
-                if(error >= 0) { // if the error is greater than or equal to zero:
-                x1 += direction; // increase the minor axis (x)
-                error += diff_double_deltas;
-                } else  {
-                    error += double_delta_x;
-                }
-                y1 += 1; // increase major axis to next pixel
-                putpixel(screen, x1 as usize, y1 as usize); // plot our pixel
-                delta_y -= 1;
-            }
-        }
-    }
-}
-
 fn main() {
     let cube = SimpleObject::cube(5);
     let trig = SineCosineTable::new(360*4);
@@ -308,7 +226,7 @@ fn main() {
                 let screen_x = (dx * rotated_point.x) / rotated_point.z + (WIN_WIDTH/2) as f64;
                 let screen_y = (dy * rotated_point.y) / rotated_point.z + (WIN_HEIGHT/2) as f64;
 
-                putpixel(&mut screen, screen_x as usize, screen_y as usize);
+                screen.putpixel(screen_x as usize, screen_y as usize);
             }
         }).unwrap();
 
